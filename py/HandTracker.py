46b561b85844b2c0c120bfab354802577bc92e18
KirillHit <khitushkink@mail.ru>
2023-06-07 18:00:10 +0300

    Update HandTracker.py
    Влад попросил

--- py/HandTracker.py
+++ py/HandTracker.py
@@ -8,18 +8,48 @@ mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
 cap = cv2.VideoCapture(0)
+cv2.namedWindow("HandTracker")
 success, image = cap.read()
 if not success:
     print("Ignoring empty camera frame.")
     exit()
 height, width = image.shape[:2]
 
+HandTracker = Ho.HandTracker(height, width)
+'''
+def ChangeCalibDist(x):
+    HandTracker.CalibDist = x
+cv2.createTrackbar("CalibDist", "HandTracker", 500, 2000, ChangeCalibDist)
+
+def ChangeCalibCam(x):
+    HandTracker.CalibCam = x/100
+cv2.createTrackbar("CalibCam/100", "HandTracker", 83, 200, ChangeCalibCam)
+
+def ChangeCamAngle(x):
+    HandTracker.CamAngle = x * 3.14 / 180
+cv2.createTrackbar("CamAngle", "HandTracker", 90, 180, ChangeCamAngle)
+
+def ChangeTimeApprox(x):
+    HandTracker.TimeApprox = x
+cv2.createTrackbar("TimeApprox", "HandTracker", 300, 2000, ChangeTimeApprox)
+
+def ChangeTimeApprox(x):
+    HandTracker.TimeApprox = x
+    HandTracker.LenApprox = 60 * x // 1000
+cv2.createTrackbar("TimeApprox", "HandTracker", 300, 2000, ChangeTimeApprox)
+
+def ChangeFixedParam(x):
+    HandTracker.FixedParam = x
+cv2.createTrackbar("FixedParam", "HandTracker", 3, 20, ChangeFixedParam)
+
+def ChangeFixed_z_Modif(x):
+    HandTracker.FixedParam = x
+cv2.createTrackbar("Fixed_z_Modif", "HandTracker", 4, 20, ChangeFixed_z_Modif)
+'''
 prev_time = 0
 
 # ДОДЕЛАТЬ
 PrecisionParam = 2 # Устрание связи зависимости точности от разрешения камеры
-
-HandTracker = Ho.HandTracker(height, width)
 
 with mp_hands.Hands(
         model_complexity=1,
@@ -87,7 +117,7 @@ with mp_hands.Hands(
         cv2.putText(image, f"FPS: {str(fps)}", (5, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 1)
         cv2.circle(image, (width//2, height//2), 20, (0, 255, 0), 1)
 
-        cv2.imshow('MediaPipe Hands', image)
+        cv2.imshow('HandTracker', image)
         if cv2.waitKey(5) & 0xFF == 27:
             break
 cap.release()
